dcab2bb92e74997ad33515ad64112f9f6e749134
juliocarneiro <juliocarneiro@gmail.com>
2023-01-30 17:19:40 -0300

    v2

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,6 +2,5 @@ import { startDevServer } from '@cypress/vite-dev-server'
 
 module.exports = (on, config) => {
   on('dev-server:start', async (options) => startDevServer({ options }))
-
   return config
 }
